b824d56ee0374e5ebf5b8c108aa9c5916f937cab
Thananjaya <thananjaya091993@gmail.com>
2019-03-13 16:55:14 +0530

    added similar posts functionality

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
 from .models import Post
 from .forms import SharePostForm, CommentForm
 from taggit.models import Tag
@@ -13,7 +14,7 @@ def post_index(request, tag_slug=None):
 		arguement:
 			request: http request
 	"""
-	post_lists = Post.objects.filter(status="published")
+	post_lists = Post.objects.filter(status = "published")
 	tag = None
 	if tag_slug:
 		tag = get_object_or_404(Tag, slug = tag_slug)
@@ -53,7 +54,10 @@ def post_show(request, year, month, day, post):
 	else:
 		comment_form = CommentForm()
 
-	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form, 'new_comment': new_comment })
+	post_tags_id = post.tags.values_list('id', flat = True)
+	similar_posts = Post.objects.filter(status = 'published', tags__in = post_tags_id).exclude(id = post.id)
+	similar_posts = similar_posts.annotate(same_tags = Count('tags')).order_by('-same_tags', "-publish")
+	return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form, 'new_comment': new_comment, 'similar_posts':similar_posts })
 
 
 def share_post(request, post_id):
