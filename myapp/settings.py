6b824cd5157ea7ec3d0a2cd506f6546499c2b5b8
Thananjaya <thananjaya091993@gmail.com>
2019-03-16 20:44:03 +0530

    added postgres

--- myapp/settings.py
+++ myapp/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoblog',
+        'USER': 'thananj',
+        'PASSWORD': 'password@1234',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
